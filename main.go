7f61f472029d8a23993169550256c3a23586e8b7
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2015-09-04 19:40:03 +0800

    Use HTTPS and HTTP/2
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- main.go
+++ main.go
@@ -8,42 +8,15 @@ import (
 
 	"github.com/bbhmakerlab/debug"
 	"github.com/bbhmakerlab/ikea-catalogue-giveaway-2016/store"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
+	"github.com/bradfitz/http2"
 	"github.com/lib/pq"
 )
 
 const (
 	MaxEntries = 8000
 
-	NameMinLength     = 6
-	NameMaxLength     = 32
-	Address1MinLength = 6
-	Address1MaxLength = 64
 	PostalCodeLength  = 5
-	CityMinLength     = 3
-	CityMaxLength     = 32
-	StateMinLength    = 5
-	StateMaxLength    = 15
-	EmailMinLength    = 3
-	EmailMaxLength    = 254
 )
-
-var States = []string{
-	"Johor",
-	"Kedah",
-	"Kelantan",
-	"Malacca",
-	"Negeri Sembilan",
-	"Pahang",
-	"Perak",
-	"Perlis",
-	"Penang",
-	"Sabah",
-	"Sarawak",
-	"Selangor",
-	"Terengganu",
-}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	r.Header.Set("Content-Type", "text/html")
@@ -78,28 +51,21 @@ func submit(w http.ResponseWriter, r *http.Request) {
 
 	// Name
 	name := r.FormValue("name")
-	if len(name) < NameMinLength {
+	if len(name) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Name length should be at least " + itoa(NameMinLength) + " characters long"))
-		return
-	} else if len(name) > NameMaxLength {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Name length should not exceed " + itoa(NameMaxLength) + " characters long"))
+		w.Write([]byte("Name shouldn't be empty"))
 		return
 	}
 
 	// Address 1
 	address1 := r.FormValue("address1")
-	if len(address1) < NameMinLength {
+	if len(address1) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Address 1 length should be at least " + itoa(Address1MinLength) + " characters long"))
-		return
-	} else if len(address1) > NameMaxLength {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Address 1 length should not exceed " + itoa(Address1MaxLength) + " characters long"))
+		w.Write([]byte("Address 1 shouldn't be empty"))
 		return
 	}
 
+	// Address 2
 	address2 := r.FormValue("address2")
 
 	// Postal Code
@@ -112,29 +78,17 @@ func submit(w http.ResponseWriter, r *http.Request) {
 
 	// City
 	city := r.FormValue("city")
-	if len(city) < CityMinLength {
+	if len(city) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("City length should be at least " + itoa(CityMinLength) + " characters long"))
-		return
-	} else if len(city) > CityMaxLength {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("City length should not exceed " + itoa(CityMaxLength) + " characters long"))
+		w.Write([]byte("City shouldn't be empty"))
 		return
 	}
 
 	// State
 	state := r.FormValue("state")
-	if len(state) < StateMinLength {
+	if len(state) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("State length should be at least " + itoa(StateMinLength) + " characters long"))
-		return
-	} else if len(state) > StateMaxLength {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("State length should not exceed " + itoa(StateMaxLength) + " characters long"))
-		return
-	} else if !isValidState(state) {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("State length is invalid"))
+		w.Write([]byte("State shouldn't be empty"))
 		return
 	}
 
@@ -148,13 +102,9 @@ func submit(w http.ResponseWriter, r *http.Request) {
 
 	// Email
 	email := r.FormValue("email")
-	if len(email) < EmailMinLength {
+	if len(email) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Email length should be at least " + itoa(EmailMinLength) + " characters long"))
-		return
-	} else if len(email) > EmailMaxLength {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Email length should not exceed " + itoa(EmailMaxLength) + " characters long"))
+		w.Write([]byte("Address 1 shouldn't be empty"))
 		return
 	}
 
@@ -167,6 +117,7 @@ func submit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Insert to database
 	if err := store.InsertEntry(name, address1, address2, city, state, country, postalCode, email); err != nil {
 		e, ok := err.(*pq.Error)
 		if ok && e.Code == "23505" { // unique violation
@@ -182,18 +133,25 @@ func submit(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/success", 302)
 }
 
-func isValidState(state string) bool {
-	for _, v := range States {
-		if state == v {
-			return true
-		}
-	}
-	return false
-}
-
 func itoa(i int) string {
 	return strconv.Itoa(i)
 }
+
+func startServer() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	srv := http.Server{Addr: ":443"}
+	http2.ConfigureServer(&srv, &http2.Server{})
+	go func() {
+	     debug.Fatal(http.ListenAndServe(":" + port, nil))
+	}()
+
+	debug.Fatal(srv.ListenAndServeTLS("/home/website/ssl/server.crt", "/home/website/ssl/server.key"))
+}
+
 
 func main() {
 	// parse CLI options
@@ -203,20 +161,11 @@ func main() {
 	store.Init()
 
 	// configure server
-	router := mux.NewRouter()
-	router.HandleFunc("/", home)
-	router.HandleFunc("/submit", submit)
-	router.HandleFunc("/success", success)
-	router.HandleFunc("/duplicate", duplicate)
-	router.HandleFunc("/failed", failed)
-	router.HandleFunc("/outofstock", outofstock)
-
-	// start server
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	n := negroni.Classic()
-	n.UseHandler(router)
-	n.Run(":" + port)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/success", success)
+	http.HandleFunc("/duplicate", duplicate)
+	http.HandleFunc("/failed", failed)
+	http.HandleFunc("/outofstock", outofstock)
+	startServer()
 }
